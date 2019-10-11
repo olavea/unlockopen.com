df0cdd3e26f379762162d37f77b3c98c8ba1f777
Ola Vea <ola@olavea.com>
2019-10-11 15:14:23 +0200

    made BasicLayout into a functional component

--- src/components/BasicLayout.js
+++ src/components/BasicLayout.js
@@ -7,42 +7,37 @@ import ColorLine from "./ColorLine"
 
 import { rhythm, scale } from "../utils/typography"
 
-class BasicLayout extends React.Component {
-  render() {
-    const children = this.props.children
-    // Should these children maybe come in higher up like?
-    return (
-      <div>
-        <Helmet>
-          <html lang="en" />
-          <meta charSet="utf-8" />
-        </Helmet>
-        <Header />
+const BasicLayout = ({ children }) => {
+  return (
+    <div>
+      <Helmet>
+        <html lang="en" />
+        <meta charSet="utf-8" />
+      </Helmet>
+      <Header />
 
-        <div
-          style={{
-            marginLeft: "auto",
-            marginRight: "auto",
-            maxWidth: rhythm(24),
-            padding: `${rhythm(1.5)} ${rhythm(3 / 4)} ${rhythm(1)} ${rhythm(
-              3 / 4
-            )}`,
-          }}
-        >
-          {children}
-        </div>
-        <Footer
-          style={{
-            marginLeft: "auto",
-            marginRight: "auto",
-            maxWidth: rhythm(24),
-            padding: `${rhythm(1)} ${rhythm(3 / 4)} 0 ${rhythm(3 / 4)}`,
-          }}
-        />
-        <ColorLine />
+      <div
+        style={{
+          marginLeft: "auto",
+          marginRight: "auto",
+          maxWidth: rhythm(24),
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)} ${rhythm(1)} ${rhythm(
+            3 / 4
+          )}`,
+        }}
+      >
+        {children}
       </div>
-    )
-  }
+      <Footer
+        style={{
+          marginLeft: "auto",
+          marginRight: "auto",
+          maxWidth: rhythm(24),
+          padding: `${rhythm(1)} ${rhythm(3 / 4)} 0 ${rhythm(3 / 4)}`,
+        }}
+      />
+      <ColorLine />
+    </div>
+  )
 }
-
 export default BasicLayout
