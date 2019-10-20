937d582895f19838d6d56d65973ef71abc62ef90
Benedicte Raae <raae@bgraphic.no>
2019-10-20 11:29:27 +0200

    Refactor Footer as functional component.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,12 @@
 import React from "react";
-// import Link from "gatsby-link";
-// import { rhythm } from "../utils/typography";
 
-class Footer extends React.Component {
-  render() {
-    const style = this.props.style;
-    return (
-      <footer style={style}>
-        <hr />
-        <p style={{ textAlign: "center" }}>
-          Copyright 2009—2019 UnlockOpen LLC
-        </p>
-      </footer>
-    );
-  }
-}
+const Footer = ({ style }) => {
+  return (
+    <footer style={style}>
+      <hr />
+      <p style={{ textAlign: "center" }}>Copyright 2009—2019 UnlockOpen LLC</p>
+    </footer>
+  );
+};
 
 export default Footer;
